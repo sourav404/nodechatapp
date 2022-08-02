83a9c76ebf78604b0d255d06121202adf7bc94d8
Sourav <60809550+sourav404@users.noreply.github.com>
2022-08-02 13:26:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.get('/',(rq,res)=>{
+  res.sendFile(__dirname + '/page.html');
+
+});
+
+
+
+io.on('connection', (socket) => {
+
+ 
+
+
+
+
+
+io.emit('count', socket.client.conn.server.clientsCount);
+
+
+
+
+
+
+
+socket.on('join', (msg) => {
+      io.emit('addjoin', msg);
+});
+
+
+
+socket.on('user', (msg) => {
+    console.log('message: ' + msg.msg);
+      io.emit('chat', msg);
+  });
+
+
+
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  
+  });
+
+
+});
+
+
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
